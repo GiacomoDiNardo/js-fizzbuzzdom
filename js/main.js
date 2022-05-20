c7dd2edc68b67d67f32749dd0e4323b9af30354d
GiacomoDiNardo <vest_95@hotmail.it>
2022-05-20 15:52:11 +0200

    Milestone 3

--- js/main.js
+++ js/main.js
@@ -6,21 +6,21 @@ for (let i = 1; i <= 100; i++) {
     // se entrambe le condizioni sono vere
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
-        numberContainer.innerHTML += `<div class="col-2">FizzBuzz</div>`
+        numberContainer.innerHTML += `<div class="square fizz-buzz-color">FizzBuzz</div>`
 
     // se è solo multiplo di 5
     } else if (i % 5 === 0) {
         console.log("Buzz");
-        numberContainer.innerHTML += `<div class="col-2">Buzz</div>`
+        numberContainer.innerHTML += `<div class="square buzz-color">Buzz</div>`
 
     // se è solo multiplo di 3
     } else if (i % 3 === 0) {
         console.log("Fizz");
-        numberContainer.innerHTML += `<div class="col-2">Fizz</div>`
+        numberContainer.innerHTML += `<div class="square fizz-color">Fizz</div>`
 
     // se non è multiplo di 5 o 3
     } else {
         console.log(i);
-        numberContainer.innerHTML += `<div class="col-2">${i}</div>`
+        numberContainer.innerHTML += `<div class="square number-color">${i}</div>`
     }
 }
